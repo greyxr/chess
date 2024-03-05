10b7eb45f2040adeec1abf900839618062aba631
David Kartchner <dk329@byu.edu>
2024-03-04 22:10:15 -0700

    Began work on SQLGameDAO

--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -1,4 +1,58 @@
 package dataAccess;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 public class SQLAuthDAO {
+
+//    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+//        try (var conn = DatabaseManager.getConnection()) {
+//            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+//                for (var i = 0; i < params.length; i++) {
+//                    var param = params[i];
+//                    if (param instanceof String p) ps.setString(i + 1, p);
+//                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+//                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
+//                    else if (param == null) ps.setNull(i + 1, NULL);
+//                }
+//                ps.executeUpdate();
+//
+//                var rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    return rs.getInt(1);
+//                }
+//
+//                return 0;
+//            }
+//        } catch (DataAccessException e) {
+//            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+//        }
+//    }
+//
+//    private final String[] createStatements = {
+//            """
+//            CREATE TABLE IF NOT EXISTS  users (
+//              `id` int NOT NULL AUTO_INCREMENT,
+//              `name` varchar(256) NOT NULL,
+//              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+//              `json` TEXT DEFAULT NULL,
+//              PRIMARY KEY (`id`),
+//              INDEX(type),
+//              INDEX(name)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+//            """
+//    };
+//
+//
+//    private void configureDatabase() throws ResponseException {
+//        DatabaseManager.createDatabase();
+//        try (var conn = DatabaseManager.getConnection()) {
+//            for (var statement : createStatements) {
+//                try (var preparedStatement = conn.prepareStatement(statement)) {
+//                    preparedStatement.executeUpdate();
+//                }
+//            }
+//        } catch (SQLException ex) {
+//            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+//        }
+//    }
 }
--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -1,60 +1,89 @@
 package dataAccess;
 
-import exceptions.BadRequestException;
-import model.UserData;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.GameData;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
-public class SQLUserDAO implements UserDAO {
+public class SQLGameDAO implements GameDAO {
 
-    public SQLUserDAO() throws DataAccessException {
+    public SQLGameDAO() throws DataAccessException {
         configureDatabase();
     }
 
     @Override
-    public UserData createUser(UserData user) throws DataAccessException, BadRequestException {
-        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
-        String statement = "INSERT INTO users (username, password, email) values (?, ?, ?)";
-        executeUpdate(statement, user.username(), hashedPassword, user.email());
-        return user;
+    public void clearGames() throws DataAccessException {
+        String statement = "DELETE * FROM games";
+        executeUpdate(statement);
+    }
+
+    private GameData readGame(ResultSet resultSet) throws SQLException {
+        int gameID = resultSet.getInt("game_id");
+        String whiteUsername = resultSet.getString("white_username");
+        String blackUsername = resultSet.getString("black_username");
+        String gameName = resultSet.getString("game_name");
+        ChessGame game = new Gson().fromJson(resultSet.getString("chess_game"), ChessGame.class);
+
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, game);
     }
 
     @Override
-    public UserData getUser(String username) throws DataAccessException {
-        UserData user = null;
+    public Collection<GameData> getGames() throws DataAccessException {
+        var result = new ArrayList<GameData>();
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT username, password FROM users WHERE username = ?";
+            var statement = "SELECT * FROM games";
             try (var ps = conn.prepareStatement(statement)) {
-                ps.setString(1, username);
                 try (var rs = ps.executeQuery()) {
-                        user = new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                    while (rs.next()) {
+                        result.add(readGame(rs));
+                    }
                 }
             }
         } catch (Exception e) {
             throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
         }
-        return user;
+        return result;
     }
 
     @Override
-    public void clearUsers() throws DataAccessException {
+    public GameData getGame(int gameID) throws DataAccessException {
+        GameData result = null;
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "DELETE from users WHERE username = *";
+            var statement = "SELECT * FROM games WHERE game_id = ?";
             try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
                 try (var rs = ps.executeQuery()) {
-                    return;
+                    result = readGame(rs);
                 }
             }
         } catch (Exception e) {
             throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
         }
+        return result;
     }
 
-    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+    @Override
+    public void insertUser(int gameID, String username, String color) throws DataAccessException {
+
+    }
+
+    @Override
+    public void insertGame(int gameID, String gameName) throws DataAccessException {
+
+    }
+
+    @Override
+    public int getBiggestGameId() throws DataAccessException {
+        return 0;
+    }
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
                 for (var i = 0; i < params.length; i++) {
@@ -69,9 +98,10 @@ public class SQLUserDAO implements UserDAO {
 
                 var rs = ps.getGeneratedKeys();
                 if (rs.next()) {
-                    rs.getInt(1);
+                    return rs.getInt(1);
                 }
 
+                return 0;
             }
         } catch (SQLException e) {
             throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
@@ -80,11 +110,13 @@ public class SQLUserDAO implements UserDAO {
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS users (
-              `username` TEXT NOT NULL,
-              `password` TEXT NOT NULL,
-              `email` TEXT DEFAULT NULL,
-              PRIMARY KEY (`username`),
+            CREATE TABLE IF NOT EXISTS games (
+              `game_id` int NOT NULL AUTO_INCREMENT,
+              `game_name` TEXT NOT NULL,
+              `white_username` TEXT,
+              `black_username` TEXT,
+              `chess_game` TEXT NOT NULL,
+              PRIMARY KEY (`game_id`),
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
